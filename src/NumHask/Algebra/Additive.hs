56153eca5bc1232be438e1fddb25cd986252c3c3
James Cranch <cranch@cantab.net>
2026-01-04 12:44:29 +0000

    "Divisible" and "Subtractable"
    We introduce classes for safe subtraction (which is often only a
    partial operation, for example with types like Natural) and safe
    division (which is even more commonly only a partial operation, as
    with Int).
    
    The main advantage to the user is that (checked, safe) exact division
    is now available: 8/2 will give 4, but 7/2 will throw an error.

--- src/NumHask/Algebra/Additive.hs
+++ src/NumHask/Algebra/Additive.hs
@@ -5,17 +5,22 @@ module NumHask.Algebra.Additive
     sum,
     accsum,
     Subtractive (..),
-    subtract
+    subtract,
+    Subtractable (..),
+    SubtractableTotal (..),
+    SubtractablePositive (..),
   )
 where
 
 import Data.Int (Int16, Int32, Int64, Int8)
+import Data.Maybe (isJust)
 import Data.Semigroup (Semigroup (..))
 import Data.Traversable (mapAccumL)
 import Data.Word (Word, Word16, Word32, Word64, Word8)
 import GHC.Natural (Natural (..))
 import NumHask.Data.Interop (FromBase (..))
-import Prelude (Bool, Double, Eq, Float, Int, Integer, Ord, Show, flip, fromInteger)
+import Prelude (Bool(..), Double, Eq, Float, Int, Integer, Maybe(..), Ord(..), Show,
+                fromInteger, otherwise, (<$>), (.), flip)
 import Prelude qualified as P
 
 -- $setup
@@ -89,21 +94,88 @@ accsum = P.snd P.. mapAccumL (\a b -> (a + b, a + b)) zero
 --
 -- >>> 1 - 2
 -- -1
-class (Additive a) => Subtractive a where
-  {-# MINIMAL (-) | negate #-}
+class Additive a => Subtractive a where
 
   negate :: a -> a
   negate a = zero - a
 
   infixl 6 -
+  -- | Subtraction, with an error (usually Underflow) on failure
   (-) :: a -> a -> a
   a - b = a + negate b
+
+  {-# MINIMAL (-) | negate #-}
+
 
 -- | Flipped subtraction, useful for currying
 -- >>> subtract 3 <$> [10,20,30]
 -- [7,17,27]
 subtract :: Subtractive a => a -> a -> a
 subtract = flip (-)
+
+
+-- | Subtraction with failure
+--
+-- We have a collection of methods to deal with the possibility that
+-- subtraction might be a partial operation. We interpret these
+-- methods as providing theoretical, rather than practical
+-- information. For example, an Int8 represents numbers between -128
+-- and 127, and therefore the subtraction (100)-(-100) will be
+-- calculated incorrectly. However, we have 'subtractable 100 (-100)'
+-- is 'True', because Int8 is a model of the integers (admittedly a
+-- very imperfect model), and this subtraction can be performed in the
+-- integers.
+--
+-- Nearly all Subtractive objects should be Subtractable: the
+-- exceptions tend to be very large, like 'a -> b', where it is not
+-- possable to tell whether there is a bad value or not.
+--
+-- Most Subtractable instances should be definable by "deriving
+-- via".
+class Subtractive a => Subtractable a where
+
+  {-# MINIMAL (-?) | negateMaybe #-}
+
+  -- | Subtraction, if possable
+  infixl 6 -?
+  (-?) :: a -> a -> Maybe a
+  a -? b = (a +) <$> negateMaybe b
+
+  -- | Negation, if possable
+  negateMaybe :: a -> Maybe a
+  negateMaybe = (zero -?)
+
+  subtractable :: a -> a -> Bool
+  subtractable x y = isJust (x -? y)
+
+  negatable :: a -> Bool
+  negatable = isJust . negateMaybe
+
+-- | A wrapper to assist providing 'Subtractable' instances where
+-- subtraction is total
+newtype SubtractableTotal a = SubtractableTotal {
+  getSubtractableTotal :: a
+} deriving (Additive, Subtractive)
+
+instance Subtractive a => Subtractable (SubtractableTotal a) where
+  x -? y = Just (x - y)
+  negateMaybe = Just . negate
+  negatable _ = True
+  subtractable _ _ = True
+
+newtype SubtractablePositive a = SubtractablePositive {
+  getSubtractablePositive :: a
+} deriving (Additive, Eq, Ord, Subtractive)
+
+instance (Ord a, Subtractive a) => Subtractable (SubtractablePositive a) where
+  x -? y
+    | x < y     = Nothing
+    | otherwise = Just (x - y)
+  negateMaybe x
+    | x < zero  = Nothing
+    | otherwise = Just (negate x)
+  negatable x = x >= zero
+  subtractable x y = x >= y
 
 
 instance P.Num a => Additive (FromBase a) where
@@ -114,17 +186,23 @@ instance P.Num a => Subtractive (FromBase a) where
   FromBase x - FromBase y = FromBase (x P.- y)
   negate (FromBase x) = FromBase (P.negate x)
 
+
 deriving via FromBase Double instance Additive Double
 deriving via FromBase Double instance Subtractive Double
+deriving via SubtractableTotal Double instance Subtractable Double
 
 deriving via FromBase Float instance Additive Float
 deriving via FromBase Float instance Subtractive Float
+deriving via SubtractableTotal Float instance Subtractable Float
 
 deriving via FromBase Int instance Additive Int
 deriving via FromBase Int instance Subtractive Int
+deriving via SubtractableTotal Int instance Subtractable Int
 
 deriving via FromBase Integer instance Additive Integer
 deriving via FromBase Integer instance Subtractive Integer
+deriving via SubtractableTotal Integer instance Subtractable Integer
+
 
 instance Additive Bool where
   (+) = (P.||)
@@ -132,33 +210,43 @@ instance Additive Bool where
 
 deriving via FromBase Natural instance Additive Natural
 deriving via FromBase Natural instance Subtractive Natural
+deriving via SubtractablePositive Natural instance Subtractable Natural
 
 deriving via FromBase Int8 instance Additive Int8
 deriving via FromBase Int8 instance Subtractive Int8
+deriving via SubtractableTotal Int8 instance Subtractable Int8
 
 deriving via FromBase Int16 instance Additive Int16
 deriving via FromBase Int16 instance Subtractive Int16
+deriving via SubtractableTotal Int16 instance Subtractable Int16
 
 deriving via FromBase Int32 instance Additive Int32
 deriving via FromBase Int32 instance Subtractive Int32
+deriving via SubtractableTotal Int32 instance Subtractable Int32
 
 deriving via FromBase Int64 instance Additive Int64
 deriving via FromBase Int64 instance Subtractive Int64
+deriving via SubtractableTotal Int64 instance Subtractable Int64
 
 deriving via FromBase Word instance Additive Word
 deriving via FromBase Word instance Subtractive Word
+deriving via SubtractablePositive Word instance Subtractable Word
 
 deriving via FromBase Word8 instance Additive Word8
 deriving via FromBase Word8 instance Subtractive Word8
+deriving via SubtractablePositive Word8 instance Subtractable Word8
 
 deriving via FromBase Word16 instance Additive Word16
 deriving via FromBase Word16 instance Subtractive Word16
+deriving via SubtractablePositive Word16 instance Subtractable Word16
 
 deriving via FromBase Word32 instance Additive Word32
 deriving via FromBase Word32 instance Subtractive Word32
+deriving via SubtractablePositive Word32 instance Subtractable Word32
 
 deriving via FromBase Word64 instance Additive Word64
 deriving via FromBase Word64 instance Subtractive Word64
+deriving via SubtractablePositive Word64 instance Subtractable Word64
 
 instance (Additive b) => Additive (a -> b) where
   f + f' = \a -> f a + f' a
@@ -166,4 +254,4 @@ instance (Additive b) => Additive (a -> b) where
 
 instance (Subtractive b) => Subtractive (a -> b) where
   f - f' = \a -> f a - f' a
-  negate f = negate P.. f
+  negate f = negate . f
--- src/NumHask/Algebra/Multiplicative.hs
+++ src/NumHask/Algebra/Multiplicative.hs
@@ -5,15 +5,22 @@ module NumHask.Algebra.Multiplicative
     product,
     accproduct,
     Divisive (..),
+    Divisible (..),
+    DivisibleTotal (..),
+    DivisibleField (..),
+    DivisibleDivMod (..),
   )
 where
 
+import Control.Exception (ArithException(..), throw)
 import Data.Int (Int16, Int32, Int64, Int8)
+import Data.Maybe (isJust)
 import Data.Traversable (mapAccumL)
 import Data.Word (Word, Word16, Word32, Word64, Word8)
 import GHC.Natural (Natural (..))
+import NumHask.Algebra.Additive (Additive(..))
 import NumHask.Data.Interop (FromBase (..))
-import Prelude (Double, Eq, Float, Int, Integer, Ord, Show, fromInteger)
+import Prelude (Double, Eq(..), Float, Int, Integer, Bool(..), Maybe(..), Ord, Show, fromInteger, otherwise, (<$>), (.))
 import Prelude qualified as P
 
 -- $setup
@@ -63,14 +70,14 @@ instance (Multiplicative a) => P.Monoid (Product a) where
 -- >>> product [1..5]
 -- 120
 product :: (Multiplicative a, P.Foldable f) => f a -> a
-product = getProduct P.. P.foldMap Product
+product = getProduct . P.foldMap Product
 
 -- | Compute the accumulating product of a 'Data.Traversable.Traversable'.
 --
 -- >>> accproduct [1..5]
 -- [1,2,6,24,120]
 accproduct :: (Multiplicative a, P.Traversable f) => f a -> f a
-accproduct = P.snd P.. mapAccumL (\a b -> (a * b, a * b)) one
+accproduct = P.snd . mapAccumL (\a b -> (a * b, a * b)) one
 
 -- | or [Division](https://en.wikipedia.org/wiki/Division_(mathematics\))
 --
@@ -97,6 +104,77 @@ class (Multiplicative a) => Divisive a where
   (/) :: a -> a -> a
   (/) a b = a * recip b
 
+
+-- | A class representing the fact that division is very frequently a
+-- partial operation, comparable to MaybeSubtractive.
+--
+-- Most Divisive instances should also be Divisible.
+class Divisive a => Divisible a where
+
+  {-# MINIMAL (/?) | recipMaybe #-}
+
+  -- | Subtraction, if possible
+  infixl 6 /?
+  (/?) :: a -> a -> Maybe a
+  a /? b = (a *) <$> recipMaybe b
+
+  -- | Negation, if possible
+  recipMaybe :: a -> Maybe a
+  recipMaybe = (one /?)
+
+  divisible :: a -> a -> Bool
+  divisible x y = isJust (x /? y)
+
+  invertible :: a -> Bool
+  invertible = isJust . recipMaybe
+
+
+newtype DivisibleTotal a = DivisibleTotal {
+  getDivisibleTotal :: a
+} deriving (Eq, Ord, Multiplicative, Divisive)
+
+instance Divisible a => Divisible (DivisibleTotal a) where
+  x /? y = Just (x / y)
+  recipMaybe x = Just (recip x)
+  divisible _ _ = True
+  invertible _ = True
+
+
+newtype DivisibleField a = DivisibleField {
+  getDivisibleField :: a
+} deriving (Eq, Ord, Additive, Multiplicative, Divisive)
+
+instance (Additive a, Divisible a, Eq a) => Divisible (DivisibleField a) where
+  x /? y
+    | y == zero = Nothing
+    | otherwise = Just (x/y)
+  recipMaybe x
+    | x == zero = Nothing
+    | otherwise = Just (recip x)
+  divisible _ = (/= zero)
+  invertible = (/= zero)
+
+
+newtype DivisibleDivMod a = DivisibleDivMod {
+  getDivisibleDivMod :: a
+} deriving (Eq, Ord, Additive, Multiplicative, P.Num, P.Enum, P.Real, P.Integral)
+
+instance (Additive a, Divisible a, P.Integral a) => Divisive (DivisibleDivMod a) where
+  x / y = case x /? y of
+    Just q -> q
+    Nothing -> throw DivideByZero
+  recip x = case recipMaybe x of
+    Just y -> y
+    Nothing -> throw DivideByZero
+
+instance (Additive a, Divisible a, P.Integral a) => Divisible (DivisibleDivMod a) where
+  x /? y
+    | r == zero = Just q
+    | otherwise = Nothing where
+        (q,r) = P.divMod x y
+  divisible x y = P.mod x y == zero
+
+
 instance P.Num a => Multiplicative (FromBase a) where
   FromBase x * FromBase y = FromBase (x P.* y)
   one = FromBase (P.fromInteger 1)
@@ -105,43 +183,71 @@ instance P.Fractional a => Divisive (FromBase a) where
   FromBase x / FromBase y = FromBase (x P./ y)
   recip (FromBase x) = FromBase (P.recip x)
 
+
 deriving via FromBase Double instance Multiplicative Double
 deriving via FromBase Double instance Divisive Double
+deriving via DivisibleField Double instance Divisible Double
 
 deriving via FromBase Float instance Multiplicative Float
 deriving via FromBase Float instance Divisive Float
+deriving via DivisibleField Float instance Divisible Float
 
 deriving via FromBase Int instance Multiplicative Int
+deriving via DivisibleDivMod Int instance Divisive Int
+deriving via DivisibleDivMod Int instance Divisible Int
 
 deriving via FromBase Integer instance Multiplicative Integer
+deriving via DivisibleDivMod Integer instance Divisive Integer
+deriving via DivisibleDivMod Integer instance Divisible Integer
 
 instance Multiplicative P.Bool where
   (*) = (P.&&)
   one = P.True
 
 deriving via FromBase Natural instance Multiplicative Natural
+deriving via DivisibleDivMod Natural instance Divisive Natural
+deriving via DivisibleDivMod Natural instance Divisible Natural
 
 deriving via FromBase Int8 instance Multiplicative Int8
+deriving via DivisibleDivMod Int8 instance Divisive Int8
+deriving via DivisibleDivMod Int8 instance Divisible Int8
 
 deriving via FromBase Int16 instance Multiplicative Int16
+deriving via DivisibleDivMod Int16 instance Divisive Int16
+deriving via DivisibleDivMod Int16 instance Divisible Int16
 
 deriving via FromBase Int32 instance Multiplicative Int32
+deriving via DivisibleDivMod Int32 instance Divisive Int32
+deriving via DivisibleDivMod Int32 instance Divisible Int32
 
 deriving via FromBase Int64 instance Multiplicative Int64
+deriving via DivisibleDivMod Int64 instance Divisive Int64
+deriving via DivisibleDivMod Int64 instance Divisible Int64
 
 deriving via FromBase Word instance Multiplicative Word
+deriving via DivisibleDivMod Word instance Divisive Word
+deriving via DivisibleDivMod Word instance Divisible Word
 
 deriving via FromBase Word8 instance Multiplicative Word8
+deriving via DivisibleDivMod Word8 instance Divisive Word8
+deriving via DivisibleDivMod Word8 instance Divisible Word8
 
 deriving via FromBase Word16 instance Multiplicative Word16
+deriving via DivisibleDivMod Word16 instance Divisive Word16
+deriving via DivisibleDivMod Word16 instance Divisible Word16
 
 deriving via FromBase Word32 instance Multiplicative Word32
+deriving via DivisibleDivMod Word32 instance Divisive Word32
+deriving via DivisibleDivMod Word32 instance Divisible Word32
 
 deriving via FromBase Word64 instance Multiplicative Word64
+deriving via DivisibleDivMod Word64 instance Divisive Word64
+deriving via DivisibleDivMod Word64 instance Divisible Word64
 
 instance (Multiplicative b) => Multiplicative (a -> b) where
-  f * f' = \a -> f a * f' a
+  (f * f') a = f a * f' a
   one _ = one
 
 instance (Divisive b) => Divisive (a -> b) where
-  recip f = recip P.. f
+  (f / f') a = f a / f' a
+  recip f = recip . f
